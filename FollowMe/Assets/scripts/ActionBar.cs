21468ec41683e6a129763b40752a97af623d7da1
Verena Schiffer <verena.schiffer@student.tugraz.at>
2016-01-29 22:35:41 +0100

    added new statusbar functions

--- FollowMe/Assets/scripts/ActionBar.cs
+++ FollowMe/Assets/scripts/ActionBar.cs
@@ -4,8 +4,7 @@ using System.Collections.Generic;
 public class ActionBar : MonoBehaviour {
 
     public List<Action> action_bar = new List<Action>();
-    //public GUISkin skin;
-
+    public GUISkin skin;
     private ActionDatabase database;
 
     
@@ -18,19 +17,35 @@ public class ActionBar : MonoBehaviour {
 	
     for(int counter = 0; counter < database.actions.Count; counter++)
     {
-      print("Curently "+database.actions[counter].actionName+" in databse");
+    	action_bar.Add(database.actions[counter]);
     }
-    /*for(int inventoryCounter = 0; inventoryCounter < database.items.Count; inventoryCounter++)
+    for(int inventoryCounter = 0; inventoryCounter < action_bar.Count; inventoryCounter++)
 	{
-	  inventory.Add(new Item());
-	}*/
+	  print("Currently "+action_bar[inventoryCounter].actionName+" in databse");
+	}
+
 
 	}
 	
 	void OnGUI ()
 	{
       //set GUI skin and Background of inventory
-	  //GUI.skin = skin;    
+	 GUI.skin = skin;
+     GUI.Box(new Rect(0,0,100,100),"",skin.GetStyle("ActionBack"));
+    // GUI.DrawTexture(paper_count_icon_rect,Resources.Load<Texture2D>("inventory icons/paper"));
+    //for(int i = 0; i < inventory.Count; i++)
+    //{
+     // tooltip = "<b>"+inventory[i].itemName +"</b>"+ "\n\n" +inventory[i].itemDesc+"\n\n"/*+ "<b><color=#ffd700>\t\t\t"+inventory[y].itemCost+" Paper"+"</color></b>"*/;
+     // Rect labelRect = GUILayoutUtility.GetRect(new GUIContent(tooltip),skin.GetStyle("Slot"));
+    //  if(labelRect.width > tooltip_rect.width)
+     // {
+     //   tooltip_rect.width = labelRect.width;
+     // }
+    //}
+		//GUI.Box(inventory_background_rect,"",skin.GetStyle("Back"));
+    
+    //GUI.Box(paper_count_rect,"",skin.GetStyle("Back"));
+    //GUI.DrawTexture(paper_count_icon_rect,Resources.Load<Texture2D>("inventory icons/paper"));    
 	}
 
 }
