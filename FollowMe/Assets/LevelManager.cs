9b69a7faf84beaae80a3387d312d82bdadb98c9a
Oskar Bechtold <bechtold@brightsolutions.de>
2016-01-30 13:25:01 +0100

    Added game manager and level manager scripts.

---
+++ FollowMe/Assets/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+
+	public void loadLevel(string levelToLoad){
+		Debug.Log ("load");
+		SceneManager.LoadScene (levelToLoad);
+	}
+}
