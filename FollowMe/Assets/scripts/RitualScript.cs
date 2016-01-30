4ee1c405d83c74f0f22ce9261f2552078666828e
Dominik Mocher <dominik.mocher@student.tugraz.at>
2016-01-30 11:34:31 +0100

    added rituals

--- FollowMe/Assets/scripts/RitualScript.cs
+++ FollowMe/Assets/scripts/RitualScript.cs
@@ -11,6 +11,16 @@ public class RitualScript : MonoBehaviour {
 	private float affiliationPlayer1;
 	private float affiliationPlayer2;
 
+	private float playerModifier;
+
+	private float repeatModifier1;
+	private float repeatModifier2;
+	private float repeatModifier3;
+	private float repeatModifier4;
+	private float repeatModifier5;
+	private float repeatModifier6;
+
+
 	// Use this for initialization
 	void Start () {
 		npcs = new List<GameObject>();
@@ -21,26 +31,51 @@ public class RitualScript : MonoBehaviour {
 
 		affiliationPlayer1 = 0.0f;
 		affiliationPlayer2 = 0.0f;
+		playerModifier = 1.0f; //MUST be 1 or -1
+
+		repeatModifier1 = 1.0f;
+		repeatModifier2 = 1.0f;
+		repeatModifier3 = 1.0f;
+		repeatModifier4 = 1.0f;
+		repeatModifier5 = 1.0f;
+		repeatModifier6 = 1.0f;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKeyDown(KeyCode.Space)) {
-			foreach (GameObject npc in npcs) 
-			{
-				npc.GetComponent<NPCAttributes>().triggerAction(5.0f, 2.0f, 1.0f);
-			}
+		if(Input.GetKeyDown(KeyCode.X)) {
+			letBeerRain();
+		}
+
+		if(Input.GetKeyDown(KeyCode.C)) {
+			slaughterLamb();
+		}
+
+		if(Input.GetKeyDown(KeyCode.V)) {
+			drought();
 		}
 
 		if(Input.GetKeyDown(KeyCode.B)) {
-			foreach (GameObject npc in npcs) 
-			{
-				npc.GetComponent<NPCAttributes>().triggerAction(-5.0f, 2.0f, 1.0f);
-			}
+			sacrificePeople();
 		}
+
+		if(Input.GetKeyDown(KeyCode.N)) {
+			praiseTheSun();
+		}
+
+		if(Input.GetKeyDown(KeyCode.M)) {
+			richHarvest();
+		}
+
 		updateStatusBars();
+
+		if(playerModifier > 0.0f){
+			Debug.Log("Player1's  (" + playerModifier + ")");
+		} else {
+			Debug.Log("Player2's turn (" + playerModifier + ")");
+		}
 	
 	}
 
@@ -65,6 +100,90 @@ public class RitualScript : MonoBehaviour {
 
 		//Debug.Log("percentage for player1: " + affiliationPlayer1);
 		//Debug.Log("percentage for player2: " + affiliationPlayer2);
+	}
 
+	//----- Rituals ----- fun-fear-noMeat
+	public void letBeerRain() {
+		//fun++
+		//fear--
+		float funValue = playerModifier * repeatModifier1 * 3.0f;
+		float fearValue = playerModifier * repeatModifier1 * -2.0f;
+		float noMeatValue = playerModifier * 1.0f;
+		foreach (GameObject npc in npcs) 
+		{		
+			npc.GetComponent<NPCAttributes>().triggerAction(funValue, fearValue, noMeatValue);
+		}
+		playerModifier *= -1.0f;
+		repeatModifier1 -= 0.25f;
+	}
+
+	public void slaughterLamb() {
+		//noMeat--
+		//fear++
+		float funValue = playerModifier * 1.0f;
+		float fearValue = playerModifier * repeatModifier2 * 3.0f;
+		float noMeatValue = playerModifier * repeatModifier2 * -2.0f;
+		foreach (GameObject npc in npcs) 
+		{		
+			npc.GetComponent<NPCAttributes>().triggerAction(funValue, fearValue, noMeatValue);
+		}
+		playerModifier *= -1.0f;
+		repeatModifier2 -= 0.25f;
+	}
+
+	public void drought() {
+		//fear++
+		//noMeat-
+		float funValue = playerModifier * 1.0f;
+		float fearValue = playerModifier * repeatModifier3 *2.5f;
+		float noMeatValue = playerModifier * repeatModifier3 * -1.5f;
+		foreach (GameObject npc in npcs) 
+		{		
+			npc.GetComponent<NPCAttributes>().triggerAction(funValue, fearValue, noMeatValue);
+		}
+		playerModifier *= -1.0f;		
+		repeatModifier3 -= 0.25f;
+	}
+
+	public void sacrificePeople() {
+		//fear++
+		//fun-
+		float funValue = playerModifier * repeatModifier4 * -2.0f;
+		float fearValue = playerModifier * repeatModifier4 * 3.0f;
+		float noMeatValue = playerModifier * 1.0f;
+		foreach (GameObject npc in npcs) 
+		{		
+			npc.GetComponent<NPCAttributes>().triggerAction(funValue, fearValue, noMeatValue);
+		}
+		playerModifier *= -1.0f;
+		repeatModifier4 -= 0.25f;
+	}
+
+	public void praiseTheSun() {
+		//fun++
+		//fear-
+		float funValue = playerModifier * repeatModifier5 * 2.5f;
+		float fearValue = playerModifier * repeatModifier5 * -1.5f;
+		float noMeatValue = playerModifier * 1.0f;
+		foreach (GameObject npc in npcs) 
+		{		
+			npc.GetComponent<NPCAttributes>().triggerAction(funValue, fearValue, noMeatValue);
+		}
+		playerModifier *= -1.0f;
+		repeatModifier5 -= 0.25f;
+	}
+
+	public void richHarvest() {
+		//noMeat++
+		//fear-
+		float funValue = playerModifier * 1.0f;
+		float fearValue = playerModifier * repeatModifier6 * -2.0f;
+		float noMeatValue = playerModifier * repeatModifier6 * 3.0f;
+		foreach (GameObject npc in npcs) 
+		{		
+			npc.GetComponent<NPCAttributes>().triggerAction(funValue, fearValue, noMeatValue);
+		}
+		playerModifier *= -1.0f;
+		repeatModifier6 -= 0.25f;
 	}
 }
